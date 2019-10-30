de12f7b936f53360cf7d58878eaaaa3887450ca9
Lucas Felipe <30655133+luksave@users.noreply.github.com>
2019-10-30 08:11:26 -0300

    REM: Acentos - 3

--- src/br/ufg/inf/mestrado/hermeswidget/client/sensor/humidity/App_HW_Humidity.java
+++ src/br/ufg/inf/mestrado/hermeswidget/client/sensor/humidity/App_HW_Humidity.java
@@ -19,7 +19,7 @@ public class App_HW_Humidity extends HermesWidgetObjects {
 		File diretorioAirPure = new File("./airPure/");
 		File registroAirPure  = new File("./airPure/medidas.csv");
 		
-		// Preparação do pool de threads de acordo com a quantidade de arquivos que contém os dados ambientais
+		// Preparacao do pool de threads de acordo com a quantidade de arquivos que contém os dados ambientais
 		ScheduledExecutorService poolWidgets = Executors.newScheduledThreadPool(diretorioAirPure.listFiles().length - 1);
 
 		/*for (File registroAtual : diretorioAirPure.listFiles()){	
